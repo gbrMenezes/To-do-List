71c30df77638c9a48f31e2eb33ef0e48cb27ba7a
Gabriel Menezes <gabrieldmfaquim0202@gmail.com>
2023-07-11 19:20:40 -0300

    javascript done

---
+++ src/script.js
@@ -0,0 +1,51 @@
+const addButton = document.querySelector('.button-add-task')
+const taskInput = document.querySelector('.input-task')
+const allList = document.querySelector('.task-list')
+
+let myTaskList = []
+
+
+
+function addNewTask() {
+    myTaskList.push({
+        task: taskInput.value,
+        done: false
+    })
+
+    taskInput.value = ''
+
+    showTasks()
+}
+
+function showTasks() {
+    
+    let newLi = '';
+
+    myTaskList.forEach((task, index) => {
+        newLi = newLi + `
+
+        <li class="task ${task.done && "done"}">
+            <img class="" src="./src/images/checked.png" alt="botão de tarefa concluída" onclick="checkTask(${index})">
+            <p>${task.task}</p>
+            <img src="./src/images/trash.png" alt="botão para apagar tarefa" onclick="deletItem(${index})">
+        </li>
+        `
+    })
+
+    allList.innerHTML = newLi
+
+}
+
+function checkTask(index) {
+    myTaskList[index].done = !myTaskList[index].done
+
+    showTasks()
+}
+
+function deletItem(index) {
+    myTaskList.splice(index, 1)
+
+    showTasks()
+}
+
+addButton.addEventListener('click', addNewTask)
